6ce2461eef0305eb7d94f3612a2b81c09e701d73
ericduboc <progeric@yahoo.com.br>
2022-04-05 23:35:41 -0300

    Update index.php
    Teste

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>Teste PHP</title>
  </head>
  <body>
-   <?php echo "Teste simples, alterado, em PHP"; ?>
+    <?php echo "Teste simples em PHP"; ?>
+    <?php echo "Outro echo em PHP"; ?>
  </body>
 </html>
