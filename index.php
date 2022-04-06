9a1e458bf440358eb8fd2fa2ac40a2af0f6bef6e
ericduboc <progeric@yahoo.com.br>
2022-04-05 23:56:30 -0300

    Update index.php
    Adicionada quebra de linha entre echo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>Teste PHP</title>
  </head>
  <body>
-    <?php echo "Teste simples em PHP"; ?>
+    <?php echo "Teste simples em PHP</br>"; ?>
     <?php echo "Outro echo em PHP"; ?>
  </body>
 </html>
